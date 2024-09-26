0b9540c739d05549758a85d95a0e3bbfd76500de
Joao Sant Ana <jmpsantana15@gmail.com>
2024-09-25 21:46:22 -0300

    feat: completed de the popup for desktop mode of the images TODO: integrate comments, create login(wait for the backend)

--- js/images.js
+++ js/images.js
@@ -2,8 +2,9 @@ const $postsArea = document.querySelector('#postsArea');
 const $postPopup = document.querySelector('#postDetails');
 const $postPopupBackground = document.querySelector('#postDetailsBackground');
 let $posts;
+let comment;
 
-async function getImages() {
+async function getPosts() {
     const response = await fetch('http://localhost:3000/posts', {
         method: 'GET',
         headers: {
@@ -15,10 +16,23 @@ async function getImages() {
     return data;
 }
 
-async function renderImages() {
-    const data = await getImages();
+async function getComments() {
+    const response = await fetch('http://localhost:3000/comments', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
 
-    data.forEach(post => {
+    const data = await response.json();
+
+    return data;
+}
+
+async function renderImages() {
+    const posts = await getPosts();
+
+    posts.forEach(post => {
         $postsArea.innerHTML += `
             <div class="post ${post.direction}" id="${post.id}">
                 <img src="${post.img}" alt="${post.title}">
@@ -28,10 +42,13 @@ async function renderImages() {
 
     $posts = document.querySelectorAll('.post');
 
-    $posts.forEach(post => {
-        post.addEventListener('click', () => {
-            openPost(data[post.id - 1]);
-        });
+    $postsArea.addEventListener('click', (event) => {
+        const postElement = event.target.closest('.post');
+        
+        if (postElement) {
+            const postId = postElement.id;
+            openPost(posts[postId - 1]);
+        }
     });
 }
 
@@ -45,15 +62,54 @@ function openPost(post) {
                 <h1>${post.title}</h1>
                 <img src="../assets/img/close.svg" alt="Fechar" onclick="closePost()" class="close">
             </div> 
-            <img src="${post.img}" alt="${post.title}">
-            <div>
-                <p>${post.likes}</p>
-                <p>10</p>
+            <img src="${post.img}" alt="${post.title}" class="post-img">
+            <div class="post-details">
+                <div>
+                    <img src="../assets/img/heartIcon.svg" alt="Comentario" class="heart">
+                    <p>${post.likes} Curtidas</p>
+                </div>
+                <div>
+                    <img src="../assets/img/chatIcon.svg" alt="Comentarios" class="chat">
+                    <p>10</p>
+                </div>    
             </div>
             <hr />
-            <input type="text" placeholder="Escreva um comentário...">
+            <div class="comment-wrapper">
+                <form method="post">
+                    <img src="../assets/img/chatIcon.svg" alt="Comentario" class="chat">
+                    <input type="text" placeholder="Escreva um comentário...">
+                    <button type="submit" id="send">
+                        <img src="../assets/img/planeIcon.svg" alt="Enviar" class="plane">
+                    </button>
+                </form>
+            </div>
+            <hr />
         </div>
     `
+}
+
+function closePost() {
+    $postPopupBackground.style.display = 'none';
+    $postPopup.style.display = 'none';
+}
+
+async function sendComment(comment) {
+    //TODO: Implementar o envio do comentário
+
+    if (!comment) {
+        return 'Comentário não pode ser vazio.';
+    }
+
+    if (comment.length > 200) {
+        return 'Comentário muito grande. Tente um comentário menor.';
+    }
+
+    if (comment.length < 2) {
+        return 'Comentário muito pequeno. Tente um comentário maior que 2 caracteres.';
+    }
+
+
+    // return response.status;
 }
 
 renderImages();
